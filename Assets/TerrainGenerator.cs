4f5e7a946ced489e656cc589945d7712cecbbcbd
Alistair Miles <ac.counter@hotmail.com>
2016-12-10 22:51:41 +0000

    Applies Perin Noise to the terrain mesh and adds a collider

--- Assets/TerrainGenerator.cs
+++ Assets/TerrainGenerator.cs
@@ -3,30 +3,34 @@ using System.Collections;
 
 public class TerrainGenerator : MonoBehaviour {
     MeshFilter mf;
-    int width = 100;
-    int height = 100;
-    int rows = 6;
-    int columns = 6;
+    float width = 100f;
+    float height = 100f;
+    int rows = 50;
+    int columns = 50;
 
 	// Use this for initialization
 	void Start () {
+        int adjRows = rows + 1;
+        int adjColumns = columns + 1;
         mf = GetComponent<MeshFilter>();
-        Vector3[] vertices = new Vector3[rows * columns];
-        int[] triangles = new int[(columns - 1) * (rows - 1) * 6];
+        Vector3[] vertices = new Vector3[adjRows * adjColumns];
+        int[] triangles = new int[(adjRows - 1) * (adjColumns - 1) * 6];
         int triangleIndex = 0;
         int vertexIndex = 0;
-        for (int row = 0; row < rows; row++)
+        for (int row = 0; row < adjRows; row++)
         {
-            for (int column = 0; column < columns; column++)
+            for (int column = 0; column < adjColumns; column++)
             {
-                vertices[(row * columns) + column] = new Vector3(column * (width / columns), 0, row * (height / rows));
-                if (row < rows - 1 && column < columns - 1)
+                float amplitude = Mathf.PerlinNoise(10 * column / width,10 * row / height);
+                amplitude = -ScaleRange(amplitude, 0, 1, 0, 20);
+                vertices[(row * adjColumns) + column] = new Vector3(column * (width / columns), amplitude, row * (height / rows));
+                if (row < adjRows - 1 && column < adjColumns - 1)
                 {
                     triangles[triangleIndex++] = vertexIndex;
-                    triangles[triangleIndex++] = vertexIndex + columns + 1;
-                    triangles[triangleIndex++] = vertexIndex + columns;
+                    triangles[triangleIndex++] = vertexIndex + adjColumns + 1;
+                    triangles[triangleIndex++] = vertexIndex + adjColumns;
                     
-                    triangles[triangleIndex++] = vertexIndex + columns + 1;
+                    triangles[triangleIndex++] = vertexIndex + adjColumns + 1;
                     triangles[triangleIndex++] = vertexIndex;
                     triangles[triangleIndex++] = vertexIndex + 1;
                 }
@@ -35,6 +39,9 @@ public class TerrainGenerator : MonoBehaviour {
         }
         mf.mesh.vertices = vertices;
         mf.mesh.triangles = triangles;
+        mf.mesh.RecalculateNormals();
+        mf.mesh.RecalculateBounds();
+        gameObject.AddComponent<MeshCollider>();
 
     }
 	
@@ -42,5 +49,12 @@ public class TerrainGenerator : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    public static float ScaleRange(float value, float oldMin, float oldMax, float newMin, float newMax)
+    {
+        float oldVal = value / (oldMax - oldMin);
+        float newVal = oldVal * (newMax - newMin);
+        return newVal;
+    }
 
 }
