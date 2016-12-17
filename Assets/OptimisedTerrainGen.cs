02fccff34833d26d93d314e44cdfe96b3795dc19
Alistair Miles <ac.counter@hotmail.com>
2016-12-17 17:24:20 +0000

    Carries out Diamond Square noise algorithm on plane (WIP: Bugs galore)

--- Assets/OptimisedTerrainGen.cs
+++ Assets/OptimisedTerrainGen.cs
@@ -9,6 +9,7 @@ public class OptimisedTerrainGen : MonoBehaviour
     public int TriColumns;
     public Material MeshMaterial;
     public float Amplitude;
+    public float Smoothness;
 
     // Use this for initialization
 	void Start ()
@@ -21,7 +22,7 @@ public class OptimisedTerrainGen : MonoBehaviour
 
         Vector3[] vertices = PlaneGen(triColumns, size);
         int[] triangles = TriGen(vertices);
-        //vertices = DiamondSquareGen(vertices);
+        vertices = DiamondSquareGen(vertices);
         //Vector2[] uvs = UVGen(vertices);
 
         GameObject go = new GameObject();
@@ -99,20 +100,97 @@ public class OptimisedTerrainGen : MonoBehaviour
 
     private Vector3[] DiamondSquareGen(Vector3[] vertices)
     {
+        Vector3[] plane = (Vector3[])vertices.Clone();
         int totalVertices = vertices.Length;
         int matrixDimensions = (int) Mathf.Sqrt(totalVertices);
         int triColumns = matrixDimensions / 2;
         // Calculate the number of required iterations
         int iterations = (int)Mathf.Log(triColumns, 2);
+
+        // Create an array to keep track of set and unset vertices
+        Boolean[] set = new Boolean[vertices.Length];
+        for (int b = 0; b < set.Length; b++)
+        {
+            set[b] = false;
+        }
+
         // Start with 4 random values at the corners
-        /*vertices[0].y = UnityEngine.Random.value * scale;
-        plane[(int)sideLength - 1].y = UnityEngine.Random.value * scale;
-        plane[plane.Length - (int)sideLength].y = UnityEngine.Random.value * scale;
-        plane[plane.Length - 1].y = UnityEngine.Random.value * scale;*/
+        plane[0].y = UnityEngine.Random.value * Amplitude; // Top left
+        plane[matrixDimensions - 1].y = UnityEngine.Random.value * Amplitude; // Top right
+        plane[(matrixDimensions - 1) * matrixDimensions].y = UnityEngine.Random.value * Amplitude; // Bottom left
+        plane[(matrixDimensions * matrixDimensions) - 1].y = UnityEngine.Random.value * Amplitude; // Bottom right
+
+        // For each iteration
+        for (int i = 0; i < 2; i++)
+        {
+            // Set the smoothness of the current iteration
+            float iterScale = Amplitude / ((i * Smoothness) + 1);
+
+            // Size the sampling matrix
+            int sampleColumns = (int)Mathf.Pow(2, i);
+            int sampleDimensions = matrixDimensions / sampleColumns;
+            int minSampleCorner = 0;
+            int row = 0;
+            // for each sub iteration (0 to 4^iteration)
+            for (int j = 0; j < (int)Mathf.Pow(4, i);j++)
+            {
+                int maxSampleCorner = (minSampleCorner + sampleDimensions - 1) + ((sampleDimensions - 1) * matrixDimensions);
+
+                // Sample the diamond
+                float ul = plane[minSampleCorner].y;
+                float ur = plane[minSampleCorner + sampleDimensions - 1].y;
+                float bl = plane[minSampleCorner + ((sampleDimensions -1) * matrixDimensions)].y;
+                float br = plane[minSampleCorner + sampleDimensions - 1 + ((sampleDimensions - 1) * matrixDimensions)].y;
+                float avg = (ul + ur + bl + br) / 4;
+
+                // Set the center 4 vertices (4x duplicates included)
+                float randVal = avg + ((UnityEngine.Random.value - 0.5f) * iterScale);
+                plane[minSampleCorner + (sampleDimensions / 2) + (((sampleDimensions / 2) - 1) * matrixDimensions) -1].y = randVal; //ul
+                plane[minSampleCorner + (sampleDimensions / 2) + (((sampleDimensions / 2)-1) * matrixDimensions)].y = randVal; // ur
+                plane[minSampleCorner + (sampleDimensions / 2) + ((sampleDimensions / 2) * matrixDimensions)].y = randVal; //br
+                plane[minSampleCorner + (sampleDimensions/2) + ((sampleDimensions/2)*matrixDimensions) - 1].y = randVal; //bl
+
+                // Sample the square
+                // Set the 8 midpoint vertices (2x duplicates included)
+
+                
+                // Top midpoints
+                randVal = UnityEngine.Random.value;
+                float val = plane[((2 * minSampleCorner) + sampleDimensions) / 2].y = ((ul + ur) / 2) + ((randVal - 0.5f) * iterScale);
+                plane[(((2 * minSampleCorner) + sampleDimensions) / 2) - 1].y = val;
+
+                // Right midpoints
+                randVal = UnityEngine.Random.value;
+                val = plane[(minSampleCorner + sampleDimensions - 1) + (((sampleDimensions / 2) - 1) * matrixDimensions)].y = ((ur + br) / 2) + ((randVal - 0.5f) * iterScale);
+                plane[(minSampleCorner + sampleDimensions - 1) + ((sampleDimensions / 2) * matrixDimensions)].y = val;
+
+                // Bottom midpoints
+                randVal = UnityEngine.Random.value;
+                val = plane[maxSampleCorner - (sampleDimensions / 2)].y = ((bl + br) / 2) + ((randVal - 0.5f) * iterScale);
+                plane[maxSampleCorner - (sampleDimensions / 2) + 1].y = val;
+
+                // Left midpoints
+                randVal = UnityEngine.Random.value;
+                val = plane[minSampleCorner + (sampleDimensions / 2 - 1) * matrixDimensions].y = ((ul + bl) / 2) + ((randVal - 0.5f) * iterScale);
+                plane[minSampleCorner + (sampleDimensions / 2) * matrixDimensions].y = val;
 
 
+                // Increment the sampling matrix
+                int rowMax = (row * matrixDimensions) + ((row + 1) * (matrixDimensions -1));
+                if (minSampleCorner + sampleDimensions - 1 == rowMax)
+                {
+                    row += sampleDimensions;
+                    minSampleCorner = (row * matrixDimensions);
+                }
+                else
+                {
+                    minSampleCorner += sampleDimensions;
+                }
+            }
+        }
+        
 
-        throw new System.NotImplementedException();
+        return plane;
     }
 
 
