59eb8d79e240c45390158720ba9fbe2d553a601e
Alistair Miles <ac.counter@hotmail.com>
2017-05-29 14:17:05 +0100

    Project over. Diamond square is not meant for infinite generation and creates too many artifacts.

--- Assets/OptimisedTerrainGen.cs
+++ Assets/OptimisedTerrainGen.cs
@@ -14,13 +14,15 @@ public class OptimisedTerrainGen : MonoBehaviour
     void Start()
     {
         GameObject p = CreatePlane(Size, TriColumns);
+
+        p.SetActive(true);
     }
+
 
     private GameObject CreatePlane(float size, int triColumns)
     {
         Vector3[] vertices = PlaneGen(triColumns, size);
         int[] triangles = TriGen(vertices);
-        vertices = DiamondSquareGen(vertices);
         Vector2[] uvs = UvGen(vertices);
 
         GameObject go = new GameObject();
@@ -34,6 +36,7 @@ public class OptimisedTerrainGen : MonoBehaviour
         mf.mesh.RecalculateBounds();
         go.AddComponent<MeshCollider>();
         mr.material = MeshMaterial;
+        go.SetActive(false);
         return go;
     }
 
@@ -61,6 +64,133 @@ public class OptimisedTerrainGen : MonoBehaviour
                 vertIndex += 2;
             }
             vertIndex += matrixDimensions;
+        }
+
+
+        // Calculate the number of required iterations
+        int iterations = (int) Mathf.Log(triColumns, 2);
+
+        // Create an array to keep track of set and unset vertices
+        Boolean[] set = new Boolean[vertices.Length];
+        for (int b = 0; b < set.Length; b++)
+        {
+            set[b] = false;
+        }
+
+        // Start with 4 random values at the corners
+        vertices[0].y = Random.value * Amplitude; // Top left
+        vertices[matrixDimensions - 1].y = Random.value * Amplitude; // Top right
+        vertices[(matrixDimensions - 1) * matrixDimensions].y = Random.value * Amplitude; // Bottom left
+        vertices[matrixDimensions * matrixDimensions - 1].y = Random.value * Amplitude; // Bottom right
+
+        // For each iteration
+        for (int i = 0; i < iterations; i++)
+        {
+            // Set the smoothness of the current iteration
+            float iterScale = Amplitude / (i * Smoothness + 1);
+
+            // Size the sampling matrix
+            int sampleColumns = (int) Mathf.Pow(2, i);
+            int sampleDimensions = matrixDimensions / sampleColumns;
+            int minSampleCorner = 0;
+            int row = 0;
+            // for each sub iteration (0 to 4^iteration)
+            for (int j = 0; j < (int) Mathf.Pow(4, i); j++)
+            {
+                int maxSampleCorner = minSampleCorner + sampleDimensions - 1 +
+                                      (sampleDimensions - 1) * matrixDimensions;
+
+                // Sample the diamond
+                float ul = vertices[minSampleCorner].y;
+                float ur = vertices[minSampleCorner + sampleDimensions - 1].y;
+                float bl = vertices[minSampleCorner + (sampleDimensions - 1) * matrixDimensions].y;
+                float br = vertices[minSampleCorner + sampleDimensions - 1 + (sampleDimensions - 1) * matrixDimensions]
+                    .y;
+                float avg = (ul + ur + bl + br) / 4;
+
+                // Set the center 4 vertices (4x duplicates included)
+                float randVal = avg + (Random.value - 0.5f) * iterScale;
+                int[] cent =
+                    GetDuplicateVertices(
+                        minSampleCorner + sampleDimensions / 2 + (sampleDimensions / 2 - 1) * matrixDimensions - 1,
+                        matrixDimensions);
+
+                foreach (int v in cent)
+                {
+                    vertices[v].y = randVal;
+                }
+
+
+                // Sample the square
+                // Set the 16 midpoint vertices (4x duplicates included)
+
+                // Top midpoints
+                randVal = Random.value;
+                int[] midN = GetDuplicateVertices((2 * minSampleCorner + sampleDimensions) / 2 - 1 - matrixDimensions,
+                    matrixDimensions);
+                float val = (ul + ur) / 2 + (randVal - 0.5f) * iterScale;
+                if (row != 0)
+                {
+                    vertices[midN[0]].y = val;
+                    vertices[midN[1]].y = val;
+                }
+                vertices[midN[2]].y = val;
+                vertices[midN[3]].y = val;
+
+
+                // Right midpoints
+                randVal = Random.value;
+                int[] midE =
+                    GetDuplicateVertices(
+                        minSampleCorner + sampleDimensions - 1 + (sampleDimensions / 2 - 1) * matrixDimensions,
+                        matrixDimensions);
+                val = (ur + br) / 2 + (randVal - 0.5f) * iterScale;
+                if (maxSampleCorner % matrixDimensions != matrixDimensions - 1)
+                {
+                    vertices[midE[1]].y = val;
+                    vertices[midE[3]].y = val;
+                }
+                vertices[midE[0]].y = val;
+                vertices[midE[2]].y = val;
+
+
+                // Bottom midpoints
+                randVal = Random.value;
+                int[] midS = GetDuplicateVertices(maxSampleCorner - sampleDimensions / 2, matrixDimensions);
+                val = (bl + br) / 2 + (randVal - 0.5f) * iterScale;
+                if (maxSampleCorner < matrixDimensions * (matrixDimensions - 1))
+                {
+                    vertices[midS[2]].y = val;
+                    vertices[midS[3]].y = val;
+                }
+                vertices[midS[0]].y = val;
+                vertices[midS[1]].y = val;
+
+                // Left midpoints
+                randVal = Random.value;
+                int[] midW = GetDuplicateVertices(minSampleCorner + (sampleDimensions / 2 - 1) * matrixDimensions - 1,
+                    matrixDimensions);
+                val = (ul + bl) / 2 + (randVal - 0.5f) * iterScale;
+                if (minSampleCorner % matrixDimensions != 0)
+                {
+                    vertices[midW[0]].y = val;
+                    vertices[midW[2]].y = val;
+                }
+                vertices[midW[1]].y = val;
+                vertices[midW[3]].y = val;
+
+
+                // Increment the sampling matrix
+                if (maxSampleCorner % matrixDimensions == matrixDimensions - 1)
+                {
+                    row += sampleDimensions;
+                    minSampleCorner = row * matrixDimensions;
+                }
+                else
+                {
+                    minSampleCorner += sampleDimensions;
+                }
+            }
         }
         return vertices;
     }
@@ -106,7 +236,7 @@ public class OptimisedTerrainGen : MonoBehaviour
                 int br = (row + 1) * matrixDimensions + column + 1;
 
                 float avg = (vertices[ul].y + vertices[ur].y + vertices[bl].y + vertices[br].y) / 4f / Amplitude;
-                int offset = 10 - Mathf.RoundToInt(avg * 10) -1;
+                int offset = 10 - Mathf.RoundToInt(avg * 10) - 1;
 
 
                 uvs[ul] = new Vector2(offset * 0.0625f, 1);
@@ -118,142 +248,8 @@ public class OptimisedTerrainGen : MonoBehaviour
         return uvs;
     }
 
-    private Vector3[] DiamondSquareGen(Vector3[] vertices)
-    {
-        Vector3[] plane = (Vector3[]) vertices.Clone();
-        int totalVertices = vertices.Length;
-        int matrixDimensions = (int) Mathf.Sqrt(totalVertices);
-        int triColumns = matrixDimensions / 2;
-        // Calculate the number of required iterations
-        int iterations = (int) Mathf.Log(triColumns, 2);
 
-        // Create an array to keep track of set and unset vertices
-        Boolean[] set = new Boolean[vertices.Length];
-        for (int b = 0; b < set.Length; b++)
-        {
-            set[b] = false;
-        }
-
-        // Start with 4 random values at the corners
-        plane[0].y = Random.value * Amplitude; // Top left
-        plane[matrixDimensions - 1].y = Random.value * Amplitude; // Top right
-        plane[(matrixDimensions - 1) * matrixDimensions].y = Random.value * Amplitude; // Bottom left
-        plane[matrixDimensions * matrixDimensions - 1].y = Random.value * Amplitude; // Bottom right
-
-        // For each iteration
-        for (int i = 0; i < iterations; i++)
-        {
-            // Set the smoothness of the current iteration
-            float iterScale = Amplitude / (i * Smoothness + 1);
-
-            // Size the sampling matrix
-            int sampleColumns = (int) Mathf.Pow(2, i);
-            int sampleDimensions = matrixDimensions / sampleColumns;
-            int minSampleCorner = 0;
-            int row = 0;
-            // for each sub iteration (0 to 4^iteration)
-            for (int j = 0; j < (int) Mathf.Pow(4, i); j++)
-            {
-                int maxSampleCorner = minSampleCorner + sampleDimensions - 1 +
-                                      (sampleDimensions - 1) * matrixDimensions;
-
-                // Sample the diamond
-                float ul = plane[minSampleCorner].y;
-                float ur = plane[minSampleCorner + sampleDimensions - 1].y;
-                float bl = plane[minSampleCorner + (sampleDimensions - 1) * matrixDimensions].y;
-                float br = plane[minSampleCorner + sampleDimensions - 1 + (sampleDimensions - 1) * matrixDimensions].y;
-                float avg = (ul + ur + bl + br) / 4;
-
-                // Set the center 4 vertices (4x duplicates included)
-                float randVal = avg + (Random.value - 0.5f) * iterScale;
-                int[] cent =
-                    GetDuplicateVertices(
-                        minSampleCorner + sampleDimensions / 2 + (sampleDimensions / 2 - 1) * matrixDimensions - 1,
-                        matrixDimensions);
-
-                foreach (int v in cent)
-                {
-                    plane[v].y = randVal;
-                }
-
-
-                // Sample the square
-                // Set the 16 midpoint vertices (4x duplicates included)
-
-                // Top midpoints
-                randVal = Random.value;
-                int[] midN = GetDuplicateVertices((2 * minSampleCorner + sampleDimensions) / 2 - 1 - matrixDimensions,
-                    matrixDimensions);
-                float val = (ul + ur) / 2 + (randVal - 0.5f) * iterScale;
-                if (row != 0)
-                {
-                    plane[midN[0]].y = val;
-                    plane[midN[1]].y = val;
-                }
-                plane[midN[2]].y = val;
-                plane[midN[3]].y = val;
-
-
-                // Right midpoints
-                randVal = Random.value;
-                int[] midE =
-                    GetDuplicateVertices(
-                        minSampleCorner + sampleDimensions - 1 + (sampleDimensions / 2 - 1) * matrixDimensions,
-                        matrixDimensions);
-                val = (ur + br) / 2 + (randVal - 0.5f) * iterScale;
-                if (maxSampleCorner % matrixDimensions != matrixDimensions - 1)
-                {
-                    plane[midE[1]].y = val;
-                    plane[midE[3]].y = val;
-                }
-                plane[midE[0]].y = val;
-                plane[midE[2]].y = val;
-
-
-                // Bottom midpoints
-                randVal = Random.value;
-                int[] midS = GetDuplicateVertices(maxSampleCorner - sampleDimensions / 2, matrixDimensions);
-                val = (bl + br) / 2 + (randVal - 0.5f) * iterScale;
-                if (maxSampleCorner < matrixDimensions * (matrixDimensions - 1))
-                {
-                    plane[midS[2]].y = val;
-                    plane[midS[3]].y = val;
-                }
-                plane[midS[0]].y = val;
-                plane[midS[1]].y = val;
-
-                // Left midpoints
-                randVal = Random.value;
-                int[] midW = GetDuplicateVertices(minSampleCorner + (sampleDimensions / 2 - 1) * matrixDimensions - 1,
-                    matrixDimensions);
-                val = (ul + bl) / 2 + (randVal - 0.5f) * iterScale;
-                if (minSampleCorner % matrixDimensions != 0)
-                {
-                    plane[midW[0]].y = val;
-                    plane[midW[2]].y = val;
-                }
-                plane[midW[1]].y = val;
-                plane[midW[3]].y = val;
-
-
-                // Increment the sampling matrix
-                if (maxSampleCorner % matrixDimensions == matrixDimensions - 1)
-                {
-                    row += sampleDimensions;
-                    minSampleCorner = row * matrixDimensions;
-                }
-                else
-                {
-                    minSampleCorner += sampleDimensions;
-                }
-            }
-        }
-
-
-        return plane;
-    }
-
-    int[] GetDuplicateVertices(int upperLeftIndex, int matrixDimensions)
+    private static int[] GetDuplicateVertices(int upperLeftIndex, int matrixDimensions)
     {
         int[] results = new int[4];
         results[0] = upperLeftIndex;
